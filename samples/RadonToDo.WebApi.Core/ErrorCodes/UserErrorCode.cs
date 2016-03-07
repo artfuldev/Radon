f7375de5d4eb60fbbcfc13d9bd25d9e0e6a87c14
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-03-07 22:58:55 +0530

    Added Error Codes

---
+++ samples/RadonToDo.WebApi.Core/ErrorCodes/UserErrorCode.cs
@@ -0,0 +1,11 @@
+﻿namespace RadonToDo.WebApi.Core.ErrorCodes
+{
+    [ErrorCodePrefix(ErrorCodePrefixes.User)]
+    public enum UserErrorCode
+    {
+        Unknown,
+        CannotCreate,
+        NotFound,
+        InvalidLogin
+    }
+}

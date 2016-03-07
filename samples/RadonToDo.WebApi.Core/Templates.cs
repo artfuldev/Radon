f6b88556cb01141c9d782c3a7c51baffa83474c1
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-03-07 23:03:00 +0530

    Added templates

---
+++ samples/RadonToDo.WebApi.Core/Templates.cs
@@ -0,0 +1,15 @@
+﻿using Radon.UriTemplates;
+
+namespace RadonToDo.WebApi.Core
+{
+    public static class Templates
+    {
+        public static readonly UriTemplate UserExistence = new UriTemplate("/users/exists{?login}");
+        public static readonly UriTemplate AccessTokens = new UriTemplate("/users/{login}/accesstokens");
+        public static readonly UriTemplate AccessToken = new UriTemplate("/users/{login}/accesstokens/{token}");
+        public static readonly UriTemplate Users = new UriTemplate("/users");
+        public static readonly UriTemplate User = new UriTemplate("/users/{login}");
+        public static readonly UriTemplate ToDoList = new UriTemplate("/users/{login}/todos");
+        public static readonly UriTemplate ToDoListItem = new UriTemplate("/users/{login}/todos/{id}");
+    }
+}

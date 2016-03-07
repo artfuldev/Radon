4fd2debfc8515522995e93cf30c32d2580919af2
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-03-07 23:03:14 +0530

    Added MediaTypes

---
+++ samples/RadonToDo.WebApi.Core/MediaTypes.cs
@@ -0,0 +1,15 @@
+﻿namespace RadonToDo.WebApi.Core
+{
+    public static class MediaTypes
+    {
+        public const string AccessToken = RadonToDo + "AccessToken" + Json;
+        public const string AccessTokenCollection = RadonToDo + "AccessTokenCollection" + Json;
+        public const string ApplicationJson = "application/json";
+        public const string Root = RadonToDo + "WebApi.Root" + Json;
+        public const string User = RadonToDo + "User" + Json;
+        public const string ToDoListItem = RadonToDo + "ToDoListItem" + Json;
+        public const string ToDoList = RadonToDo + "ToDoList" + Json;
+        private const string Json = "+json";
+        public const string RadonToDo = "application/vnd.RadonToDo.";
+    }
+}

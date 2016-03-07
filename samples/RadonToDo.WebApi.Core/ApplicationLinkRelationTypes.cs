c169b6ac5d5aa32b3ad13bf5076779ab1d4cee7d
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-03-07 23:06:53 +0530

    Added link relation types

---
+++ samples/RadonToDo.WebApi.Core/ApplicationLinkRelationTypes.cs
@@ -0,0 +1,17 @@
+﻿namespace RadonToDo.WebApi.Core
+{
+    public static class ApplicationLinkRelationTypes
+    {
+        public const string GetAccessToken = "get-access-token";
+        public const string AccessTokens = "access-tokens";
+        public const string CreateAccessToken = "create-access-token";
+        public const string User = "user";
+        public const string GetUser = "get-user";
+        public const string GetUserExistence = "get-user-existence";
+        public const string CreateUser = "create-user";
+        public const string ToDoList = "todos";
+        public const string ToDoListItem = "todos-item";
+        public const string CompleteToDoListItem = "complete-todos-item";
+        public const string GetToDoList = "get-todos";
+    }
+}

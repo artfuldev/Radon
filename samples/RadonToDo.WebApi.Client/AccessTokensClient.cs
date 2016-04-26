13bec4513f041a0f399a6e3f3fd913a4e2199bf3
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-04-26 21:39:12 +0530

    Added AccessTokens Client

---
+++ samples/RadonToDo.WebApi.Client/AccessTokensClient.cs
@@ -0,0 +1,12 @@
+﻿using Radon.Client;
+using Radon.Client.Http;
+
+namespace RadonToDo.WebApi.Client
+{
+    public class AccessTokensClient : ApiClient, IAccessTokensClient
+    {
+        public AccessTokensClient(IApiConnection apiConnection) : base(apiConnection)
+        {
+        }
+    }
+}
